c146602870fae20e63e05f90bb70de936ab447fb
kavitha sundarraj <jeyakumar.kavitha@gmail.com>
2020-11-06 12:58:08 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/*Generics
+ * Date : 06 NOV 2020
+ * a class Box<> that can store anything. 
+It should have two public methods:
+•	void Add(element)
+•	element Remove()
+•	int Count { get; }
+Adding should add on top of its contents. Remove should get the topmost element. */
+
+namespace Generics
+{
+    class Box<T>
+    {
+        private List<T> Data = new List<T>();
+        public void Add(T element)
+        {
+            Data.Add(element);
+        }
+        public int Count()
+        {
+            return Data.Count;
+        }
+        public T Remove()
+        {
+            return Data[Data.Count -1];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+/*Generics
+ * Date : 06 NOV 2020
+ * a class Box<> that can store anything.  */
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BOX");
+            //using int
+            Box<int> box1 = new Box<int>();
+            box1.Add(1);
+            box1.Add(2);
+            box1.Add(3);
+            Console.WriteLine(box1.Remove());
+            box1.Add(4);
+            box1.Add(5);
+            Console.WriteLine(box1.Remove());
+            //using sting
+            Box<String> box2 = new Box<String>();
+            box2.Add("Dog");
+            box2.Add("Cat");
+            box2.Add("Lion");
+            Console.WriteLine(box2.Remove());
+            box2.Add("Tiger");
+            box2.Add("Wolf");
+            Console.WriteLine(box2.Remove());
+        }
+
+    }
+}
+
